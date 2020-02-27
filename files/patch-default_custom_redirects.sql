e14fc1859a86bf6881f92f5e19e49aa769472d2b
1o1o1 <46845416+1o1o1@users.noreply.github.com>
2020-02-27 22:37:50 +0200

    Add files via upload

---
+++ files/patch-default_custom_redirects.sql
@@ -0,0 +1,5 @@
+--- share/sql/default_custom_redirects.sql.orig
++++ share/sql/default_custom_redirects.sql
+@@ -1,1 +1,1 @@ SET
+-INSERT IGNORE INTO custom_redirects (address, flag, enable) VALUES ('hub.verlihub.net:7777', '0', '1')
++INSERT IGNORE INTO custom_redirects (address, flag, enable) VALUES ('dchub.in.ua:420', '0', '1')
