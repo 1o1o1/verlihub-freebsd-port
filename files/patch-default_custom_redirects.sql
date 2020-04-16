72c743fd040d1d2e3fe70c50c35184aa1c0f331a
1o1o1 <46845416+1o1o1@users.noreply.github.com>
2020-04-17 00:56:30 +0300

    Update patch-default_custom_redirects.sql

--- files/patch-default_custom_redirects.sql
+++ files/patch-default_custom_redirects.sql
@@ -1,5 +1,8 @@
 --- share/sql/default_custom_redirects.sql.orig
 +++ share/sql/default_custom_redirects.sql
-@@ -1,1 +1,1 @@ SET
--INSERT IGNORE INTO custom_redirects (address, flag, enable) VALUES ('hub.verlihub.net:7777', '0', '1')
-+INSERT IGNORE INTO custom_redirects (address, flag, enable) VALUES ('dchub.in.ua:420', '0', '1')
+@ -2,1 +2,1 @@ SET
+-       ('hub.verlihub.net:7777'),
++       ('dchub.in.ua:420'),
+@@ -3,1 +3,1 @@ SET
+-       ('nmdcs://hub.verlihub.net:7777')
++       ('nmdcs://dchub.in.ua:420')
