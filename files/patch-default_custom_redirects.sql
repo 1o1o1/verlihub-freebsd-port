749eb804c2a4c34298c174b1b8d555403756c109
1o1o1 <46845416+1o1o1@users.noreply.github.com>
2021-09-17 02:00:11 +0300

    update custom redirects

--- files/patch-default_custom_redirects.sql
+++ files/patch-default_custom_redirects.sql
@@ -1,8 +1,8 @@
 --- share/sql/default_custom_redirects.sql.orig
 +++ share/sql/default_custom_redirects.sql
-@ -2,1 +2,1 @@ SET
--       ('hub.verlihub.net:7777'),
-+       ('dchub.in.ua:420'),
+@@ -2,1 +2,1 @@ SET
+-	('hub.verlihub.net:7777'),
++	('dchub.in.ua:420'),
 @@ -3,1 +3,1 @@ SET
--       ('nmdcs://hub.verlihub.net:7777')
-+       ('nmdcs://dchub.in.ua:420')
+-	('nmdcs://hub.verlihub.net:7777')
++	('nmdc://dchub.in.ua:420')
