d16f07600ed3b8042ad111261351b63d020c6268
1o1o1 <46845416+1o1o1@users.noreply.github.com>
2021-09-17 13:14:25 +0300

    update patch-default_custom_redirects.sql

--- files/patch-default_custom_redirects.sql
+++ files/patch-default_custom_redirects.sql
@@ -1,8 +1,8 @@
 --- share/sql/default_custom_redirects.sql.orig
 +++ share/sql/default_custom_redirects.sql
 @@ -2,1 +2,1 @@ SET
--	('hub.verlihub.net:7777'),
-+	('dchub.in.ua:420'),
+-	('hub.verlihub.net:7777', 0),
++	('dchub.in.ua:420', 0),
 @@ -3,1 +3,1 @@ SET
--	('nmdcs://hub.verlihub.net:7777')
-+	('nmdc://dchub.in.ua:420')
+-	('nmdcs://hub.verlihub.net:7777', 1)
++	('nmdc://dchub.in.ua:420', 1)
